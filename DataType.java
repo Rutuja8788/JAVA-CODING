a3ea13488eb02719c4300dc163d1c5a6d63b3ce8
Rutuja Pawase <rutujapawase7218@gmail.com>
2024-04-25 15:53:17 +0530

    Data Type

---
+++ DataType.java
@@ -0,0 +1,33 @@
+
+public class DataType
+{
+    public static void main(String[] args)
+    {
+        // Integer data types
+        byte byteVar = 100;
+        short shortVar = 1000;
+        int intVar = 100000;
+        long longVar = 1000000000L;
+
+
+        float floatVar = 3.14f;
+        double doubleVar = 3.14159;
+
+
+        char charVar = 'A';
+
+
+        boolean boolVar = true;
+
+
+        System.out.println("Byte Variable: " + byteVar);
+        System.out.println("Short Variable: " + shortVar);
+        System.out.println("Integer Variable: " + intVar);
+        System.out.println("Long Variable: " + longVar);
+        System.out.println("Float Variable: " + floatVar);
+        System.out.println("Double Variable: " + doubleVar);
+        System.out.println("Character Variable: " + charVar);
+        System.out.println("Boolean Variable: " + boolVar);
+    }
+}
+
