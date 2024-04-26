ce2366bfc2425bb4e87e3c6b1442b7f081eed804
Rutuja Pawase <rutujapawase7218@gmail.com>
2024-04-26 11:17:29 +0530

    for and while Loop code

---
+++ LoopExample.java
@@ -0,0 +1,18 @@
+public class LoopExample
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println("Printing numbers from 1 to 5 :");
+        for (int i = 1; i <= 5; i++)
+        {
+            System.out.println(i);
+        }
+        System.out.println("\nPrinting numbers from 1 to 5 using a while loop:");
+        int j = 1;
+        while (j <= 5) {
+            System.out.println(j);
+            j++;
+        }
+    }
+}
