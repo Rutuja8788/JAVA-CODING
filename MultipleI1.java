e71c801a254a45427c1f45dde6f503441cceedc2
Rutuja Pawase <rutujapawase7218@gmail.com>
2024-04-30 11:57:09 +0530

    Multiple_Inheritance

---
+++ MultipleI1.java
@@ -0,0 +1,41 @@
+interface API
+{
+
+    default void show()
+    {
+        System.out.println("Default API");
+    }
+}
+interface Interface1 extends API {
+
+    void display();
+}
+
+interface Interface2 extends API {
+
+    void print();
+}
+
+class Multiple1 implements Interface1, Interface2
+{
+
+    public void display()
+    {
+        System.out.println("Display from Interface1");
+    }
+
+    public void print()
+    {
+        System.out.println("Print from Interface2");
+    }
+    // Main driver method
+    public static void main(String args[])
+    {
+        Multiple1 d = new Multiple1();
+        d.show();
+        d.display();
+        d.print();
+    }
+}
+
+
