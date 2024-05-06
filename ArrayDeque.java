c981f11e26b6d195a281e2ad5621e8afae7e953f
Rutuja Pawase <rutujapawase7218@gmail.com>
2024-05-06 15:33:04 +0530

    ArrayDequeExample

---
+++ ArrayDeque.java
@@ -0,0 +1,27 @@
+public class ArrayDeque
+{
+    public static void main(String[] args)
+    {
+        java.util.ArrayDeque<Integer> de_que
+                = new java.util.ArrayDeque<Integer>(10);
+
+        de_que.add(10);
+        de_que.add(20);
+        de_que.add(30);
+        de_que.add(40);
+        de_que.add(50);
+
+        System.out.println(de_que);
+
+        de_que.clear();
+
+        de_que.addFirst(564);
+        de_que.addFirst(291);
+
+
+        de_que.addLast(24);
+        de_que.addLast(14);
+
+        System.out.println(de_que);
+    }
+}
