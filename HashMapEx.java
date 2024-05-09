2312ad1325a911f2dde37eeb6565ff378189bada
Rutuja Pawase <rutujapawase7218@gmail.com>
2024-05-09 10:11:44 +0530

    HashMapExamaple

---
+++ HashMapEx.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+// Main class
+public class HashMapEx
+{
+
+    public static void main(String[] args)
+    {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Rutuja", 10);
+        map.put("Pawase", 30);
+        map.put("saurabh", 20);
+
+
+        for (Map.Entry<String, Integer> e : map.entrySet())
+
+        {
+            System.out.println(e.getKey() + " "
+                    + e.getValue());
+        }
+    }
+}
