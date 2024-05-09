123308ce836785b995da550f3cadbe4eaf4a6936
Rutuja Pawase <rutujapawase7218@gmail.com>
2024-05-09 10:37:55 +0530

    TreeMap

---
+++ TreeMapEx.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+// Main class
+public class TreeMapEx {
+
+
+    public static void main(String[] args)
+    {
+
+
+        Map<Integer,String> map = new TreeMap<>();
+
+        map.put(10,"rutu");
+        map.put(2,null);
+        map.put(20,"saurabh");
+
+
+        for (Map.Entry<Integer,String > e : map.entrySet())
+
+
+            System.out.println(e.getKey() + " "+ e.getValue());
+    }
+}
