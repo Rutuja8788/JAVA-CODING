38310e1b17e20f629ffd6da9d2cddbd19c6f620a
Rutuja Pawase <rutujapawase7218@gmail.com>
2024-05-09 10:25:37 +0530

    linkedHashMapExamaple

---
+++ LinkedHashMap1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+// Main class
+public class LinkedHashMap1 // insertion order follow
+{
+
+    // Main driver method
+    public static void main(String[] args)
+    {
+
+
+        Map<String, Integer> map = new LinkedHashMap<>();
+
+        map.put("Rutu", 10);
+        map.put(null, null);
+        map.put("pawase", 20);
+
+
+        for (Map.Entry<String, Integer> e : map.entrySet())
+
+
+            System.out.println(e.getKey() + " " + e.getValue());
+    }
+}
