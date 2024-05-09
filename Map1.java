9d2d6d541ad757cfed9695902ec22b1880bcdd5f
Rutuja Pawase <rutujapawase7218@gmail.com>
2024-05-09 14:23:36 +0530

    MapExample1

---
+++ Map1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+import java.util.stream.Collectors;
+
+public class Map1
+{
+    public static void main(String s[])
+    {
+        List<String>s1=Arrays.asList("rutuja","pawase","masnsi");
+        List<String>s2=new ArrayList<>();
+        /*using enhanced loop
+        for(String str:s1) {
+            s2.add(str.toUpperCase());
+        }
+        System.out.println(s2);
+        */
+        //using stream api
+        s2=s1.stream().map(String::toUpperCase).collect(Collectors.toList());
+        System.out.println(s2);
+    }
+}
