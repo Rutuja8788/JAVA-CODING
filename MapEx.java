28c0e5cb064c9625c898bb7b8a4775204684242e
Rutuja Pawase <rutujapawase7218@gmail.com>
2024-05-09 10:03:21 +0530

    MapExample

---
+++ MapEx.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class MapEX {
+    public static void main(String args[])
+    {
+        Map<String, Integer> hm = new HashMap<>();
+
+        hm.put("a", 100);
+        hm.put("b", 200);
+        hm.put("c", 300);
+        hm.put("d", 400);
+        for (Map.Entry<String, Integer> entry : hm.entrySet()) //we cannot iterate map using forloop because map is not interble Collection
+        {
+            System.out.print(entry.getKey() + ":");
+            System.out.println(entry.getValue());
+        }
+    }
+}
