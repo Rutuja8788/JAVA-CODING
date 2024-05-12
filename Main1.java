f2fea355b2a0377c80304a6d720aa1e5b46476fe
Rutuja Pawase <rutujapawase7218@gmail.com>
2024-05-12 19:46:47 +0530

    GenericsExample

--- Main1.java
+++ Main1.java
@@ -1,8 +1,13 @@
 import generics.GenericsEX;
 
-public class Main1 {
-    public static void main(String[] args) {
+public class Main1
+{
+    public static void main(String[] args)
+    {
         GenericsEX< String,Integer> s1 = new GenericsEX<>(22,"rutuja",67);
+
+           s1.<String>demo("rutu");
+        s1.<Integer>demo(54);
         System.out.println(s1.getInt());
         System.out.println(s1.getT1());
         System.out.println(s1.getT2());
